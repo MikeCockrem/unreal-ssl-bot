94c65eae179de9051afc1966adb9796785928bbe
MikeCockrem <MikeCockrem@users.noreply.github.com>
2017-04-20 16:51:18 +0800

    Update LetsEnscript.pl

--- LetsEnscript.pl
+++ LetsEnscript.pl
@@ -28,10 +28,12 @@
 START
  \
  |   GetUID             Check if we're running as a user who can perform the necessary functions/access relevant files
- |   LetsEnc            Use the le.pl/Crypt::LE (ver. >21) to query for/request renewal [ quit here if cert doesnt need renewing ]
- |   AppendRC           If LetsEncrypt le.pl has provided new cert, append LE's root certificate to end (unreal needs full chain)
- |   GetHash            Extract OLD hash from expired cert and new hash from new hash and write to file
- |   PushRemote         SCP the hash file from GetHash subroutine and send to leaf node (do this early before reload)
+ |   CheckExpiry        Checks expiry date of SSL certificate and quits script early if renewal is not required.
+ |   LetsEnc            Use the le.pl/Crypt::LE (ver. >21) to query for/request renewal [ quit here if unable to renew ]
+ |     \
+ |      | AppendRC           If LetsEncrypt le.pl has provided new cert, append LE's root certificate to end (unreal needs full chain)
+ |      | GetHash            Extract OLD hash from expired cert and new hash from new hash and write to file
+ |      \ PushRemote         SCP the hash file from GetHash subroutine and send to leaf node (do this early before reload)
  |   InstallCert        Make a backup of the old file, then move the new certificate in place
  |   Rehash             System call to restart unreal; This should complete the operation.
  \
@@ -76,9 +78,9 @@ my $PERMS = 0600;                                       # File permissions for c
 my $HashFile = "HashFile.out";                          # SSL Fingerprints file
 my $Domains = "";                          # Target domain name
 my $CSRKey = "";            # Certificate Signing Request private key (LE Private key)
-my $CSRFile = "";                   # Certificate Signing Request file (CSR)
-my $PriKey = "";                       # Certificate/Domain private key
-my $ExpiryDeadline = "1 day";                           # How long should we wait to renew? (Don't forget to change --renew (n) in the LetsEnc module)
+my $CSRFile = "";                  # Certificate Signing Request file (CSR)
+my $PriKey = "";                   # Certificate/Domain private key
+my $ExpiryDeadline = "1 day";      # How long should we wait to renew? (Don't forget to change --renew (n) in the LetsEnc module)
 my $FH0;
 my $FH1;
 my $FH2;
@@ -88,71 +90,76 @@ my $ExpiryCheck;
 my $ExpireDate;
 my $Expired;
 
-#&GetUID();
-#&CheckExpiry();
-#&AppendRC();
-#&InstallCert();
+&GetUID();
+&CheckExpiry();
+&LetEnc();
+&InstallCert();
+&Rehash();
 
 sub GetHash {
-        my $OldFingerprint = qx(openssl x509 -sha256 -fingerprint -noout -in $WorkPath/$CertName | cut -d = -f2 | tr -d '\n') || die "Couldn't evaluate $WorkPath/$CertName fingerprint, aborting!\n";
-        my $NewFingerprint = qx(openssl x509 -sha256 -fingerprint -noout -in $WorkPath/$CertNameTMP | cut -d = -f2 | tr -d '\n')|| die "Couldn't evaluate $WorkPath/$CertNameTMP fingerprint, aborting!\n";
-        open ( FH2, ">", "$WorkPath/$HashFile" ) || die "Couldn't create/open $WorkPath/$HashFile for writing\n";
-        print FH2 "$OldFingerprint\n$NewFingerprint";
-        close ( FH2 ) || die "Error closing file handle FH2\n";
+    my $OldFingerprint = qx(openssl x509 -sha256 -fingerprint -noout -in $WorkPath/$CertName | cut -d = -f2 | tr -d '\n') || die "Couldn't evaluate $WorkPath/$CertName fingerprint, aborting!\n";
+    my $NewFingerprint = qx(openssl x509 -sha256 -fingerprint -noout -in $WorkPath/$CertNameTMP | cut -d = -f2 | tr -d '\n')|| die "Couldn't evaluate $WorkPath/$CertNameTMP fingerprint, aborting!\n";
+    open ( FH2, ">", "$WorkPath/$HashFile" ) || die "Couldn't create/open $WorkPath/$HashFile for writing\n";
+    print FH2 "$OldFingerprint\n$NewFingerprint";
+    close ( FH2 ) || die "Error closing file handle FH2\n";
 }
 
 sub InstallCert {
-        copy( "$WorkPath/$CertName","$WorkPath/$CertName.$NowTime" ) || die "Fatal error backing up $CertName\n";
-        move( "$WorkPath/$CertNameTMP","$WorkPath/$CertName" ) || die "Fatal error moving $CertName in to place\n";
-        chmod $PERMS, "$WorkPath/$CertName" || die "Couldn't set permissions on $CertName\n";
-        chown $UID, $GID, "$WorkPath/$CertName" || die "Couldn't set owner/group on $CertName\n";
-        qx(restorecon "$WorkPath/$CertName"); # Can't use die here, best effort.
-        unlink "$WorkPath/$CertNameTMP" || die "Couldn't clean up certificate temporary file in $WorkPath: $CertNameTMP\n";
+    copy( "$WorkPath/$CertName","$WorkPath/$CertName.$NowTime" ) || die "Fatal error backing up $CertName\n";
+    move( "$WorkPath/$CertNameTMP","$WorkPath/$CertName" ) || die "Fatal error moving $CertName in to place\n";
+    chmod $PERMS, "$WorkPath/$CertName" || die "Couldn't set permissions on $CertName\n";
+    chown $UID, $GID, "$WorkPath/$CertName" || die "Couldn't set owner/group on $CertName\n";
+    qx(restorecon "$WorkPath/$CertName"); # Can't use die here, best effort.
+    unlink "$WorkPath/$CertNameTMP" || die "Couldn't clean up certificate temporary file in $WorkPath: $CertNameTMP\n";
 }
 
 sub CheckExpiry {
-        $ExpiryCheck = Net::SSL::ExpireDate->new( file => '$WorkPath/$CertName' );
+    $ExpiryCheck = Net::SSL::ExpireDate->new( file => '$WorkPath/$CertName' );
 
-        if (defined $ExpiryCheck->ExpireDate) {
-                $ExpireDate = $ExpiryCheck->ExpireDate;
-                $Expired = $ExpiryCheck->is_expired($ExpiryDeadline);
-                if (not defined $Expired) { die "Not expired\n"; } # Leave script
-        } else {
-                die "Could not establish certificate expiry dates\n";
-        }
+    if (defined $ExpiryCheck->ExpireDate) {
+            $ExpireDate = $ExpiryCheck->ExpireDate;
+            $Expired = $ExpiryCheck->is_expired($ExpiryDeadline);
+            if (not defined $Expired) { die "Not expired\n"; } # Leave script
+    } else {
+            die "Could not establish certificate expiry dates\n";
+    }
 }
 
 sub LetsEnc {
-        if ((qx(which le.pl)) ne 0) { die "Not found\n"; }
+    if ((qx(which le.pl)) ne 0) { die "FATAL: le.pl Not found, can't continue!\n"; }
 
-        my @argsarray = (
-                '--key $WorkPath/$CSRKey',
-                '--csr $WorkPath/$CSRFile',
-                '--csr-key $WorkPath/$PriKey',
-                '--crt $WorkPath/$CertNameTMP',
-                '--domains $Domains',
-                '--handle-with Crypt::LE::Challenge::Simple',
-                '--handle-as dns',
-                '--renew 1',
-                '--issue-code=88',
-        );
-        my $args = join ' ', @argsarray;
-        qx(/usr/local/bin/le.pl $args); # Perlception
+    my @argsarray = (
+            '--key $WorkPath/$CSRKey',
+            '--csr $WorkPath/$CSRFile',
+            '--csr-key $WorkPath/$PriKey',
+            '--crt $WorkPath/$CertNameTMP',
+            '--domains $Domains',
+            '--handle-with Crypt::LE::Challenge::Simple',
+            '--handle-as dns',
+            '--renew 1',
+            '--issue-code=88',
+    );
+    my $args = join ' ', @argsarray;
+    qx(/usr/local/bin/le.pl $args); # Perlception
 
-        if ( $? eq 88) {
-                &GetHash();
-                &PushRemote();
-        } else {
-                die "Renewal not called\n";
-        }
+    # Code 88 will be returned by le.pl if the renewal was successful
+    if ( $? eq 88) {
+            &AppendRC();
+            &GetHash();
+            &PushRemote();
+    } else {
+            die "FATAL: We were within renewal window but le.pl didn't return succesful renewal code: Exiting with failure\n";
+    }
 }
 
 sub AppendRC {
-        open ( FH0, ">>", "$CertNameTMP" ) || die "Couldn't open $CertNameTMP\n";
-        open ( FH1, "<", "$RootCert" ) || die "Couldn't open $RootCert\n";
-        while ( my $readline = <FH1> ) { print FH0 $readline; }
-        close ( FH0 ) || die "Error closing file handle FH0\n";
-        close ( FH1 ) || die "Error closing file handle FH1\n";
+    open ( FH0, ">>", "$WorkPath/$CertNameTMP" ) || die "Couldn't open $WorkPath/$CertNameTMP\n";
+    open ( FH1, "<", "$WorkPath/$RootCert" ) || die "Couldn't open $WorkPath/$RootCert\n";
+    while ( my $readline = <FH1> ) { 
+        print FH0 $readline;
+    }
+    close ( FH0 ) || die "Error closing file handle FH0\n";
+    close ( FH1 ) || die "Error closing file handle FH1\n";
 }
 
 sub PushRemote {
@@ -167,9 +174,9 @@ sub GetUID {
 }
 
 sub Rehash {
-        system ( "service unreal stop" );
-        sleep (2);
-        system ( "service unreal start");
+    system ( "service unreal stop" );
+    sleep (2);
+    system ( "service unreal start");
 }
 
 sub watchdoggo {
